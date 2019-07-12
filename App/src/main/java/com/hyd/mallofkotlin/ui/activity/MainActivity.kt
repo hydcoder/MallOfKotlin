a72caee9430dbd2944d92772962fc63414764c29
hydCoder <hyd_coder@163.com>
2019-07-12 11:13:52 +0800

    搭建项目环境，创建BaseLibrary和provider模块，创建相应的包结构。

---
+++ App/src/main/java/com/hyd/mallofkotlin/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hyd.mallofkotlin.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hyd.mallofkotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
